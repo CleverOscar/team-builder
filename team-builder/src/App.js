00f2112a8ff42e4b88d5fcae0a5ee70494fb5569
Oscar Pacheco <Oscarpa88@yahoo.com>
2021-01-30 11:53:20 -0600

    Dummy Data
    Dummy data has been setup in app state

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -3,12 +3,22 @@ import React, {useState} from 'react';
 
 function App() {
 
-  const [team, setTeam] = useState({
-    id: null, 
-    name: '',
-    email: '',
-    role: ''
-  });
+  const [team, setTeam] = useState([{
+    id: 1, 
+    name: 'Oscar Ortiz',
+    email: 'oscarpa9188@gmail.com',
+    role: 'Full-Stack Developer'
+  },{
+    id: 2, 
+    name: 'Samantha Granda',
+    email: 'Samg96@gmail.com',
+    role: 'Vet Technician'
+  },{
+    id: 3, 
+    name: 'Izzy Avitan',
+    email: 'izzy429@gmail.com',
+    role: 'Cannabis Specialist'
+  }]);
 
   return (
     <div className="App">
